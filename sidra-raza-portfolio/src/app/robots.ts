6a5a3462b9eb1eeb1bc069fdb18c0664c1202a66
SidraRaza <ahed880011@gmail.com>
2026-01-14 17:17:11 +0500

    update

--- sidra-raza-portfolio/src/app/robots.ts
+++ sidra-raza-portfolio/src/app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next';
 
-export function robots(): MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
